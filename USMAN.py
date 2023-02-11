98a805a4a81949bf023a0fcef63d2b3ebc8210bb
USMAN-RAJPOOT <113978329+USMAN-RAJPOOT@users.noreply.github.com>
2023-02-11 13:01:06 +0500

    Create USMAN.py

---
+++ USMAN.py
@@ -0,0 +1,25 @@
+import os, platform
+
+try:
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+import requests
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    from HEHE import Main
+
+    Main()
+
+elif bit == '32bit':
+
+    from HEHE32 import Main
+
+    Main()
